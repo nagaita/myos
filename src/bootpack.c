684a46592e37f93143b2820c3b5789f1125a2859
nagaita <tn00gm@gmail.com>
2019-04-20 00:15:27 +0900

    Finish day4 > sec5.

--- src/bootpack.c
+++ src/bootpack.c
@@ -5,9 +5,10 @@ void write_mem8(int addr, int data);
 void HariMain(void)
 {
 	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
+	char *p;
 
 	for (i = 0xa0000; i <= 0xaffff; i++) {
-		write_mem8(i, i & 0x0f);
+		p[i] = i & 0x0f;
 	}
 
 	for (;;) {
